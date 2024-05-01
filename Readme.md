761a61242045128a6958242f587794126f775965
Anton Koleda <a.kaliada@andersenlab.com>
2024-05-01 05:56:30 +0300

    minor fixes, add readme

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Nexxiod Test Task
+
+Welcome to the Nexxiod test task repository! This guide will help you set up and run the project using Docker Compose. You can interact with the API endpoints using Swagger UI or Postman for testing.
+
+### Getting Started
+
+#### Clone the Repository
+git clone https://github.com/ankoo0/internal-test-task
+#### Navigate to Project Folder
+cd <your-project-folder>
+#### Run Docker Compose
+bash
+docker-compose up
+### Accessing the Application
+
+Once the Docker containers are up and running, you can access the API documentation and test the endpoints.
+
+#### Swagger UI
+Open your browser and navigate to [http://localhost:8080/swagger-ui/index.html](http://localhost:8080/swagger-ui/index.html) to explore and interact with the API using Swagger UI.
+
+#### Postman Collection
+Use the provided Postman collection to test the API endpoints. Import the collection into Postman and start testing the endpoints.
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.client (id, first_name, last_name, job, position, email, gender) VALUES ('20744d5a-7a58-4596-b702-c5ac68eae47e', 'cfdt', 'Gxxxa', 'Scene', 'Actor', 'some@c.com', 'MALE');
-INSERT INTO public.client (id, first_name, last_name, job, position, email, gender) VALUES ('6c51091c-f811-4838-b91e-9733aab6c7da', 'Lid', 'Goga', null, null, 'xcv@gh.cfg', 'MALE');
+INSERT INTO public.client (id, first_name, last_name, job, position, email, gender) VALUES ('6c51091c-f811-4838-b91e-9733aab6c7da', 'Lid', 'Goga', 'Aston', 'HR', 'xcv@gh.cfg', 'MALE');
 INSERT INTO public.client (id, first_name, last_name, job, position, email, gender) VALUES ('c09e2709-2c67-4471-821f-4413c9ce6365', 'Benedict', 'Cumberbatch', 'Warner Brothers', 'Actor', 'bcumb@g.co', 'MALE');
 INSERT INTO public.client (id, first_name, last_name, job, position, email, gender) VALUES ('4bdc424b-54dc-4b06-bcb6-bef5d6043ef6', 'Michael', 'Jaccino', 'Musician', 'Compositor', 'fdg@gb.cb', 'MALE');
 INSERT INTO public.client (id, first_name, last_name, job, position, email, gender) VALUES ('da327ac5-3141-4b28-8e9c-14a7fe614de2', 'Ivan', 'Petrov', 'Java Engineer', 'Team Lead', 'fdf43@gf.cb', 'MALE');
